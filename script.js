2c21b14050a0a2d9bc578866d7ae94cd57461634
Nn04nN <pip49558@gmail.com>
2026-01-16 16:53:39 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDress() {
+  const dress = document.getElementById("dress");
+  if (dress.style.display === "none") {
+    dress.style.display = "block";
+  } else {
+    dress.style.display = "none";
+  }
+}
